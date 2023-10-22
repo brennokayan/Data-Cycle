2a43972e9efeff1079b12eb23c4e26c9d1f3888b
brennokayan <brennokayan@hotmail.com>
2023-10-22 09:46:47 -0400

    correção build phosphor

--- src/constants/DefaultIcons.tsx
+++ src/constants/DefaultIcons.tsx
@@ -1,13 +1,9 @@
-import { ArrowLeft } from "@phosphor-icons/react/dist/ssr/ArrowLeft";
-import { List } from "@phosphor-icons/react/dist/ssr/List";
-import { Stack } from "@phosphor-icons/react/dist/ssr/Stack";
-import { SignOut } from "@phosphor-icons/react/dist/ssr/signout";
-import { User } from "@phosphor-icons/react/dist/ssr/User";
+import { ArrowLeft, SignOut, User, List, Stack } from "@phosphor-icons/react";
 
 export const DefaultIcons = {
-    MenuIcons : List,
-    VoltarIcon : ArrowLeft,
-    DashboardIcosn: Stack,
-    LogoutIcon: SignOut,
-    UsuarioIcon: User,
-}
+  MenuIcons: List,
+  VoltarIcon: ArrowLeft,
+  DashboardIcosn: Stack,
+  LogoutIcon: SignOut,
+  UsuarioIcon: User,
+};
